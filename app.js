815edc9c4693e06dfb47f39503571b541be01637
jeanlouisHERVE <jeanlouis.herve@hotmail.fr>
2022-01-22 10:38:12 +0100

    add places and users routes files

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 
+const placesRoutes = require('./routes/places-route');
+
 const app = express()
+
+app.use('/api/places', placesRoutes) // => 
 
 
 app.listen(5000)
