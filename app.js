2322580a60b27288360b4795cf153e0b270e9fda
jeanlouisHERVE <jeanlouis.herve@hotmail.fr>
2022-01-24 10:47:49 +0100

    add post route for place and install uuidV4

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const placesRoutes = require('./routes/places-route');
 
 const app = express()
+
+app.use(bodyParser.json())
 
 app.use('/api/places', placesRoutes) // => /api/places...
 
