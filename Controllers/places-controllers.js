d99c60470a93f6c6af2b28afea988fe8c99dad7f
jeanlouisHERVE <jeanlouis.herve@hotmail.fr>
2022-01-26 16:18:11 +0100

    another method for getPlacesByUser with populate

--- Controllers/places-controllers.js
+++ Controllers/places-controllers.js
@@ -52,10 +52,22 @@ const getPlaceById = async (req, res, next) => {
 const getPlacesByUserId = async (req, res, next) => {
     const userId = req.params.uid;
 
-    let places;
+    // let places;
+    // try {
+    //      places = await Place.find({ creator: userId });
+    // } catch {
+    //     const error = new HttpError (
+    //         'Fetching places failed, please try again later',
+    //         500
+    //     );
+    //     return next(error)
+    // }
+
+    //same method with populate
+    let userWithPlaces;
     try {
-         places = await Place.find({ creator: userId });
-    } catch {
+        userWithPlaces = await User.findById(userId).populate('places')
+    } catch (err) {
         const error = new HttpError (
             'Fetching places failed, please try again later',
             500
@@ -63,13 +75,20 @@ const getPlacesByUserId = async (req, res, next) => {
         return next(error)
     }
 
-    if(!places || places.length === 0) {
+    // if(!places || places.length === 0) {
+    //     return next (
+    //         new HttpError ('Could not find places for the provided user id', 404)
+    //     );
+    // } 
+
+    if(!userWithPlaces || userWithPlaces.places.length === 0) {
         return next (
             new HttpError ('Could not find places for the provided user id', 404)
         );
     } 
 
-    res.json({places: places.map(place => place.toObject({getters: true}))});
+    // res.json({places: places.map(place => place.toObject({getters: true}))});
+    res.json({places: userWithPlaces.places.map(place => place.toObject({getters: true}))});
 };
 
 const createPlace = async (req, res, next) => {
