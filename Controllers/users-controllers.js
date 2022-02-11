bbd55c66577bd5319d41df9470b1b23cdb62a6cb
jeanlouisHERVE <jeanlouis.herve@hotmail.fr>
2022-02-11 15:24:52 +0100

    generating tokens on the backend

--- Controllers/users-controllers.js
+++ Controllers/users-controllers.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const { get } = require("express/lib/response");
 
@@ -79,7 +80,21 @@ const signup = async (req, res, next) => {
         return next(error);
     };
 
-    res.status(201).json({user: createdUser.toObject({ getters: true})});
+    let token; 
+    try {
+        token = jwt.sign(
+            {userId: createdUser.id, email: createdUser.email}, 
+            'supersecret_dont_share', 
+            {expiresIn: '1h'}
+        );
+    } catch (err){
+        const error = new HttpError(
+            'Signing up failed, please try again',
+            500 
+        );
+        return next(error);
+    }
+    res.status(201).json({userId: createdUser.id, email: createdUser.email, token: token});
 }; 
 
 const login = async (req, res, next) => {
@@ -122,8 +137,27 @@ const login = async (req, res, next) => {
         );
         return next(error)
     }
+
+    let token; 
+    try {
+        token = jwt.sign(
+            {userId: existingUser.id, email: existingUser.email}, 
+            'supersecret_dont_share', 
+            {expiresIn: '1h'}
+        );
+    } catch (err){
+        const error = new HttpError(
+            'Logging in failed, please try again',
+            500 
+        );
+        return next(error);
+    }
     
-    res.json({message: 'Logged in', user: existingUser.toObject({ getters: true})})
+    res.json({
+        userId: existingUser.id,
+        email: existingUser.email,
+        token: token,
+    })
 }; 
 
 exports.getUsers = getUsers; 
