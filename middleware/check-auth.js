d45667a611916367466b2f3ebc873a77c037cc03
jeanlouisHERVE <jeanlouis.herve@hotmail.fr>
2022-02-16 20:50:37 +0100

    handle the options issue

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -3,7 +3,9 @@ const jwt = require('jsonwebtoken');
 const HttpError = require("../models/http-error");
 
 module.exports = (res, req, next) => {
-
+    if(req.method === 'OPTIONS') {
+        return next();
+    }
     try {
         const token = req.headers.authorization.split(' ')[1];
         if (!token) {
